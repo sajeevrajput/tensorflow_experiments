4d08adcfeb33dea189019fc0126510de195a03db
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2017-12-17 01:06:41 +0530

    linear reg demo with tensorboard code

---
+++ linreg.py
@@ -0,0 +1,23 @@
+
+
+import tensorflow as tf
+
+
+x=tf.placeholder(tf.float32,shape=(None,),name="x")
+y=tf.placeholder(tf.float32, shape=(None,),name="y")
+
+
+weights = tf.Variable(5.0, name="weights")
+biases  = tf.Variable(3.0, name="biases")
+
+loss = tf.square(tf.subtract(y,tf.add(tf.multiply(weights,x),biases)))
+
+optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter("./graphs", sess.graph)
+    for _ in range(20):
+        sess.run(optimizer,feed_dict={x:[-1,0,1,2], y:[-1,2,5,8]})
+        print(weights.eval(),biases.eval())
+    writer.close()
