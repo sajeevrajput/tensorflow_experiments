ce251fd0a19694b241d4c267e9eed59d9cfe9b4a
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2017-12-17 14:44:05 +0530

    added codes for generate data and plot

--- linreg.py
+++ linreg.py
@@ -2,6 +2,18 @@
 
 import tensorflow as tf
 
+#generates a polynomial data set of order 3
+def generate_data():
+    x = np.linspace(1,8,100)
+    y = x**3+4*x**2+5*x+np.random.random(100)*80-60
+    print(x)
+    return x,y
+
+def display_data(x,y):
+    x,y = generate_data()
+    plt.figure(1)
+    plt.scatter(x,y)
+    plt.show()
 
 x=tf.placeholder(tf.float32,shape=(None,),name="x")
 y=tf.placeholder(tf.float32, shape=(None,),name="y")
