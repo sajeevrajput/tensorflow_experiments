8c31d9d38c14247217bced397205af212615abca
Sajeev Rajput <sajeev.rajput@continental-corporation.com>
2018-04-24 21:02:16 +0530

    added custom estimator for iris

--- iris_custom_estimator.py
+++ iris_custom_estimator.py
@@ -2,6 +2,7 @@
 from six.moves.urllib.request import urlopen
 import os
 import logging
+import tensorflow as tf
 
 
 LOG_PATH = './logs'
@@ -15,7 +16,7 @@ URL_TEST = "http://download.tensorflow.org/data/iris_test.csv"
 # Log setup
 if not os.path.exists(LOG_PATH):
     os.mkdir(LOG_PATH)
-logging.basicConfig(filename=os.path.join(LOG_PATH, 'iris.log'),
+logging.basicConfig(                                    # filename=os.path.join(LOG_PATH, 'iris.log'),
                     level=logging.DEBUG,
                     format='%(levelname)-10s %(asctime)s %(message)s')
 
@@ -24,12 +25,12 @@ logging.basicConfig(filename=os.path.join(LOG_PATH, 'iris.log'),
 def download(url, file):
     if not os.path.exists(DOWNLOAD_LOCATION):
         os.mkdir(DOWNLOAD_LOCATION)
-        logging.error('Folder created')
+        logging.info('Folder created')
     if not os.path.exists(file):
         data = urlopen(url).read()
         with open(file, 'wb') as f:
             f.write(data)
-            logging.warning('Downloaded file')
+            logging.info('Downloaded file')
 
 
 download(URL_TRAIN, FILE_TRAIN_PATH)
@@ -37,7 +38,17 @@ download(URL_TEST, FILE_TEST_PATH)
 
 
 # 1. write one or more dataset importing functions
+def input_func(file):
+    dataset = tf.data.TextLineDataset(filenames=file).skip(1)
+    dataset = tf.decode_csv(dataset, [[0.], [0.], [0.], [0.], [0]])
+    label = dataset[-1]
+    features = dataset
+    print(dataset)
 
 # 2. define feature columns
 # 3. write model function
 # 4. implement training, evaluation and predictions
+
+
+if __name__ == '__main__':
+    input_func(file=FILE_TRAIN_PATH)
